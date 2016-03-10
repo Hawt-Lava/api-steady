77daf89a1be760215c65691f62192e22d27f5aa4
DanielHabib <d.g.habib7@gmail.com>
2016-03-10 16:53:36 -0500

    Initialize one - many relationship

--- api/steady/models/scoresheet.py
+++ api/steady/models/scoresheet.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from models.entry import Entry
 # Create your models here.
 
 
 class ScoreSheet(models.Model):
     """Holds onto a group of entries"""
     date = models.TimeField()
+    entries = models.ForeignKey(Entry, many=True)
