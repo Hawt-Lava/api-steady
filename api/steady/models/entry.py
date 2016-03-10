e237665927e1454ed0df3873f271ecf414a21aef
DanielHabib <d.g.habib7@gmail.com>
2016-03-10 16:50:13 -0500

    Rename question prompt

--- api/steady/models/entry.py
+++ api/steady/models/entry.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from models.prompt import Prompt
 # Create your models here.
 
 
 class Entry(models.Model):
     """An Entry refers to each input, there are multiple per spreadsheet"""
     score = models.IntegerField()
+    prompt = models.ForeignKey(Prompt, related_name='prompt')
