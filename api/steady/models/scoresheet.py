0086147ff21a550a29fecb0c1fca82a830966e5f
DanielHabib <d.g.habib7@gmail.com>
2016-03-11 13:02:04 -0500

    Score Sheet and Serializer

--- api/steady/models/scoresheet.py
+++ api/steady/models/scoresheet.py
@@ -1,12 +1,11 @@
 from django.db import models
 from api.steady.models import Entry
-# Create your models here.
 
 
 class ScoreSheet(models.Model):
     """Holds onto a group of entries"""
     date = models.TimeField()
-    entries = models.ForeignKey(Entry, default=1)
+    entries = models.ForeignKey(Entry, default=[1])
 
     class Meta:
         app_label = 'steady'
--- api/steady/serializers/scoresheet_serializer.py
+++ api/steady/serializers/scoresheet_serializer.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from api.steady.models.scoresheet import ScoreSheet
 from api.steady.models.entry import Entry
+from api.steady.serializers.entry_serializer import EntrySerializer
 
 
 class ScoreSheetSerializer(serializers.ModelSerializer):
+    entries = EntrySerializer(many=True)
+
     class Meta:
         model = ScoreSheet
 
     def create(self, validated_data):
         entries_data = validated_data.pop('entries')
-        scoreSheet = ScoreSheet.objects.create(**validated_data)
+        score_sheet = ScoreSheet.objects.create(**validated_data)
         for entry_data in entries_data:
             Entry.objects.create(**entry_data)
-        return scoreSheet
+        return score_sheet
