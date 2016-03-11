c270a90ca30d126ebf2b3626ae8f1fa5d1554100
DanielHabib <d.g.habib7@gmail.com>
2016-03-11 12:55:43 -0500

    Add Entries to scoresheet

--- api/steady/models/scoresheet.py
+++ api/steady/models/scoresheet.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from api.steady.models import Entry
 # Create your models here.
 
 
 class ScoreSheet(models.Model):
     """Holds onto a group of entries"""
     date = models.TimeField()
+    entries = models.ForeignKey(Entry, default=1)
 
     class Meta:
         app_label = 'steady'
--- api/steady/serializers/scoresheet_serializer.py
+++ api/steady/serializers/scoresheet_serializer.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from api.steady.models.scoresheet import ScoreSheet
+from api.steady.models.entry import Entry
 
 
 class ScoreSheetSerializer(serializers.ModelSerializer):
     class Meta:
         model = ScoreSheet
+
+    def create(self, validated_data):
+        entries_data = validated_data.pop('entries')
+        scoreSheet = ScoreSheet.objects.create(**validated_data)
+        for entry_data in entries_data:
+            Entry.objects.create(**entry_data)
+        return scoreSheet
