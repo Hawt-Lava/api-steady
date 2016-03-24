fdf1a176fac1940cc56bcbb6e597cd93027d6574
DanielHabib <d.g.habib7@gmail.com>
2016-03-24 15:14:26 -0400

    Test: Prompt Maintains Properties

--- api/steady/tests/models/entry_test.py
+++ api/steady/tests/models/entry_test.py
@@ -13,6 +13,9 @@ class EntryTest(TestCase):
         prompt = Prompt()
         entry = Entry()
         entry.prompt = prompt
+        prompt_text = self.faker.sentence()
+        prompt.text = prompt_text
         entry.score = score
         self.assertEquals(entry.prompt, prompt)
+        self.assertEquals(entry.prompt.text, prompt_text)
         self.assertEquals(entry.score, score)
