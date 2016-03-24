0cfc929ba073946e6f8a1ff8f46d1cd406acaad1
DanielHabib <d.g.habib7@gmail.com>
2016-03-23 22:03:51 -0400

    Test Unicode

--- api/steady/tests/models/prompt_test.py
+++ api/steady/tests/models/prompt_test.py
@@ -7,8 +7,15 @@ from unittest import TestCase
 class PromptTest(TestCase):
     def setUp(self):
         self.fake = Factory.create()
-    def testProperties(self):
+
+    def test_properties(self):
         prompt = Prompt()
         text = self.fake.sentence()
         prompt.text = text
         self.assertEquals(prompt.text, text)
+
+    def test_unicode(self):
+        prompt = Prompt()
+        text = self.fake.sentence()
+        prompt.text = text
+        self.assertEquals(str(prompt), text)
