e29c20c4b6c8077419f60ac5ad2c273dd450a964
DanielHabib <d.g.habib7@gmail.com>
2016-03-25 16:24:20 -0400

    Improve ScoreSheet Test

--- api/steady/tests/models/scoresheet_test.py
+++ api/steady/tests/models/scoresheet_test.py
@@ -1,7 +1,7 @@
 from api.steady.tests.base_test import BaseTest
 from api.steady.models.scoresheet import ScoreSheet
 from api.steady.models.entry import Entry
-
+from api.steady.models.prompt import Prompt
 
 class ScoresheetTest(BaseTest):
 
@@ -20,7 +20,38 @@ class ScoresheetTest(BaseTest):
 
         entry1.save()
         entry2.save()
-        scoresheet.entries.add(entry1)
-        scoresheet.entries.add(entry2)
+
+        scoresheet.entries.add(entry1, entry2)
 
         self.assertEquals(scoresheet.label, label)
+        self.assertEquals(len(scoresheet.entries.all()), 2)
+
+    def test_full_relationship_tree(self):
+        scoresheet = ScoreSheet()
+        label = self.faker.sentence()
+        scoresheet.label = label
+        
+        scoresheet.save()
+        
+        entry1 = Entry()
+        entry2 = Entry()
+        
+        entry1.score = 1
+        entry2.score = 2
+        
+        entry1.save()
+        entry2.save()
+        
+        prompt1 = Prompt(text=self.faker.sentence())
+        prompt2 = Prompt(text=self.faker.sentence())
+        
+        scoresheet.entries.add(entry1, entry2)
+
+        entry1.prompt = prompt1
+        entry2.prompt = prompt2
+        
+        scoresheet.save()
+
+        self.assertEquals(scoresheet.label, label)
+        self.assertEquals(len(scoresheet.entries.all()), 2)
+        // Assert Prompt and Entries are correct
