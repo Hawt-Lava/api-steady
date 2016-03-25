e7218d04ab74e712d9e398f275921cf97e066f41
DanielHabib <d.g.habib7@gmail.com>
2016-03-25 12:11:52 -0400

    Tests are failing because I am unable to set a one->Many relationship

--- api/steady/tests/models/scoresheet_test.py
+++ api/steady/tests/models/scoresheet_test.py
@@ -1,5 +1,15 @@
 from api.steady.tests.base_test import BaseTest
+from api.steady.models.scoresheet import ScoreSheet
+from api.steady.models.entry import Entry
 
 class ScoresheetTest(BaseTest):
+
     def test_properties(self):
-        pass
+       scoresheet = ScoreSheet()
+       label = self.faker.sentence()
+       scoresheet.label = label
+       entry1 = Entry()
+       entry2 = Entry()
+       scoresheet.entries = [entry1, entry2]
+       self.assertEquals(scoresheet.label, label)
+
