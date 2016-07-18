aba1465293e259c3180f3c9cbf250c0eb1c6aab6
Daniel Habib <d.g.habib7@gmail.com>
2016-07-18 08:20:03 -0400

    Record the created date of an entry

--- api/steady/models/entry.py
+++ api/steady/models/entry.py
@@ -8,6 +8,7 @@ class Entry(models.Model):
     """An Entry refers to each input, there are multiple per spreadsheet"""
     score = models.IntegerField()
     prompt = models.ForeignKey(Prompt, default=1)
+    created = models.DateTimeField(auto_now=True)
 
     class Meta:
         app_label = 'steady'
